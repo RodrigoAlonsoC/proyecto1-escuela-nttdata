c1056fc4cbd4783606d4d5d3709cc55d7ce94b83
Rodrigo Canaza <rcanaza@telbelweb.net>
2021-11-14 17:39:48 -0500

    Update clientService

--- src/main/java/com/nttdata/escuela/service/CreditService.java
+++ src/main/java/com/nttdata/escuela/service/CreditService.java
@@ -1,5 +1,6 @@
 package com.nttdata.escuela.service;
 
+import com.nttdata.escuela.App;
 import com.nttdata.escuela.repository.ChargeRepository;
 import com.nttdata.escuela.repository.CreditRepository;
 import com.nttdata.escuela.repository.PaymentRepository;
@@ -9,31 +10,17 @@ import java.util.stream.Collectors;
 /**
  * Class CreditService implement logic of Credit.
  */
-public class CreditService {
-
-    private CreditRepository creditRepository;
-    private PaymentRepository paymentRepository;
-    private ChargeRepository chargeRepository;
-
-
-    /**
-     * Constructor of Class CreditService
-     */
-    public CreditService() {
-        creditRepository = new CreditRepository();
-        paymentRepository = new PaymentRepository();
-        chargeRepository = new ChargeRepository();
-    }
+public class CreditService extends App {
 
     /**
      * Get credits of clients
      */
     public void getCredits(){
-        creditRepository.getAllBusinessCredit()
+        bin.creditRepository.getAllBusinessCredit()
                 .forEach(System.out::println);
-        creditRepository.getAllPersonalCredit()
+        bin.creditRepository.getAllPersonalCredit()
                 .forEach(System.out::println);
-        creditRepository.getAllCreditCard()
+        bin.creditRepository.getAllCreditCard()
                 .forEach(System.out::println);
     }
 
@@ -43,7 +30,7 @@ public class CreditService {
      * @param clientId
      */
     public void getCreditCardBalanceByClientId(Integer clientId){
-        creditRepository.getAllCreditCard().stream()
+        bin.creditRepository.getAllCreditCard().stream()
                 .filter(credit -> credit.getClientId().equals(clientId))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
@@ -54,7 +41,7 @@ public class CreditService {
      * @param clientId
      */
     public void getBusinessCreditByClientId(Integer clientId){
-        creditRepository.getAllBusinessCredit().stream()
+        bin.creditRepository.getAllBusinessCredit().stream()
                 .filter(credit -> credit.getBusinessClientId().equals(clientId))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
@@ -65,7 +52,7 @@ public class CreditService {
      * @param clientId
      */
     public void getPersonalCreditByClientId(Integer clientId){
-        creditRepository.getAllPersonalCredit().stream()
+        bin.creditRepository.getAllPersonalCredit().stream()
                 .filter(credit -> credit.getPersonalClientId().equals(clientId))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
@@ -76,11 +63,11 @@ public class CreditService {
      * @param creditId
      */
     public void getPaymentAndChargeByCredit(Integer creditId){
-        paymentRepository.getAll()
+        bin.paymentRepository.getAll()
                 .stream()
                 .filter(payment -> payment.getCreditId().equals(creditId))
                 .forEach(System.out::println);
-        chargeRepository.getAll()
+        bin.chargeRepository.getAll()
                 .stream()
                 .filter(charge -> charge.getCreditId().equals(creditId))
                 .forEach(System.out::println);
